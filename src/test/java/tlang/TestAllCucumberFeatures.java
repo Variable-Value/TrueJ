7439f50a3964d257908c273affed103639a05429
George S. Cowan <cowan@acm.org>
2021-01-18 15:18:05 -0500

    Allow strict option to default for Cucumber
    Default is already: strict = true

--- src/test/java/tlang/TestAllCucumberFeatures.java
+++ src/test/java/tlang/TestAllCucumberFeatures.java
@@ -4,7 +4,10 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"progress", "html:build/reports/tests/cucumber/html"}, strict = true)
+@CucumberOptions( features = "src/test/resources/tlang/Features/Spec Features"
+                , plugin = {"progress", "html:build/reports/tests/cucumber/html"}
+                )
 public class TestAllCucumberFeatures {
 }
