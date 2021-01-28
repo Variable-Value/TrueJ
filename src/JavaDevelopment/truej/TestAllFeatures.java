4876e982afdbfd739c3235495406149fd807a450
George S. Cowan <cowan@acm.org>
2021-01-28 09:26:08 -0500

    Speed up build by only testing key features

--- src/JavaDevelopment/truej/TestAllFeatures.java
+++ src/JavaDevelopment/truej/TestAllFeatures.java
@@ -1,4 +1,4 @@
-package tlang;
+package truej;
 
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features = "src/test/resources/tlang/Features/Spec Features"
+@CucumberOptions( features = "src/test/resources/tlang/Features"
                 , plugin = {"progress", "html:build/reports/tests/cucumber/html"}
                 )
-public class TestAllCucumberFeatures {
+public class TestAllFeatures {
 }
---
+++ src/test/java/tlang/TestMainCucumberFeatures.java
@@ -0,0 +1,16 @@
+package tlang;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions( features = "src/test/resources/tlang/Features/A - Spec Features"
+                , plugin = {"progress"
+                           , "html:build/reports/tests/cucumber/html/"             // Cuke 5.x
+                        // , "html:build/reports/tests/cucumber/CucumberTest.html" // Cuke 6+
+                           }
+                )
+public class TestMainCucumberFeatures {
+}
