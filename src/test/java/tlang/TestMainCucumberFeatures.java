972985d5f1d77de255732a6f49bd4fe939740919
George S. Cowan <cowan@acm.org>
2021-01-30 22:15:14 -0500

    Upgrade Cucumber to 6.8.1
    Upgrade Cucumber and improve formatting and documentation comments in build.gradle

--- src/test/java/tlang/TestMainCucumberFeatures.java
+++ src/test/java/tlang/TestMainCucumberFeatures.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions( features = "src/test/resources/tlang/Features/A - Spec Features"
                 , plugin = {"progress"
-                           , "html:build/reports/tests/cucumber/html/"             // Cuke 5.x
-                        // , "html:build/reports/tests/cucumber/CucumberTest.html" // Cuke 6+
+                        // , "html:build/reports/tests/cucumber/html/"             // Cuke 5.x
+                           , "html:build/reports/tests/cucumber/CucumberTest.html" // Cuke 6+
                            }
                 )
 public class TestMainCucumberFeatures {
