e66ffa6cba0844e5f351c8810efc8f55a777ea48
George S. Cowan <cowan@acm.org>
2022-02-28 15:16:28 -0500

    Create TestCucumberFeatures.java for using debugger with Cucumber

--- src/test/java/tlang/TestCucumberFeatures.java
+++ src/test/java/tlang/TestCucumberFeatures.java
@@ -8,12 +8,13 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions( features = "src/test/resources/tlang/"
              // , tags = "@SmokeTest"
-                , tags = "@InProgress"
+             // , tags = "@InProgress"
+                , tags = "@testthis"
                 , plugin = {"progress"
                         // , "html:build/reports/tests/cucumber/html/"             // Cuke 5.x
                         // , "html:build/reports/tests/cucumber/CucumberTest.html" // Cuke 6+
                            , "html:build/reports/tests/cucumber/FeaturesInProgress.html"
                            }
                 )
-public class TestMainCucumberFeatures {
+public class TestCucumberFeatures {
 }
