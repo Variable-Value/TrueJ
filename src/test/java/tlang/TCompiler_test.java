092b497b2bea03db530b595c9dd96f55878b0960
George S. Cowan <cowan@acm.org>
2022-02-28 15:33:51 -0500

    Test Decorated and Undecorated final-means-statements

--- src/test/java/tlang/TCompiler_test.java
+++ src/test/java/tlang/TCompiler_test.java
@@ -12,79 +12,128 @@ import org.junit.Test;
 
 public class TCompiler_test {
 
+// TODO: additional tests needed
+  // If final decoration is required from either field or local variable decoration,
+  //   then disallow undecorated local variable & vice versa
+  // Check that expected number of messages are issued
+  // CHeck TODOs
+
 @Test
-public void DecoratedFinalFieldUsedWithoutDecoration_errorTest() throws Exception {
-  String unitMarker = "-unit";
-  String compileUnit =   "  class TestClass {"
-              + "\n"+    "  int a, b;"
-              + "\n"+    "  int n' = 5;"            // LINE 3
-              + "\n"+""
-              + "\n"+    "    void testMethod() {"
-              + "\n"+    "      int startingA = n;" // ERROR LINE
-              + "\n"+    "    }"
-              + "\n"+""
-              + "\n"+    "  } // end class"
-              ;
-  String[] args = {unitMarker, compileUnit};
-  TCompiler truejCompiler = compileFresh(args);
-  msgMustContain(truejCompiler, "A different final decoration, n', was defined at line 3");
+public void decorationDisagreesWithUndecoratedUninitialized_errorTest() throws Exception {
+  compileForError(
+      """
+      class TestClass {
+        int a;
+        int n' = 5;           // ERROR LINE
+        int m';               // ERROR LINE
+        int b;
+        int c = 2;
+      } // end class
+      """ ,
+
+      """
+      Valuename n' must be undecorated to agree with the previous use of undecorated final values
+      Valuename m' must be undecorated to agree with the previous use of undecorated final values
+      """);
 }
 
 @Test
-public void UndecoratedFinalFieldUsedWithDecoration_errorTest() throws Exception {
-  String unitMarker = "-unit";
-  String compileUnit =   "  class TestClass {"
-              + "\n"+    "  int a, b;"
-              + "\n"+    "  int n = 5;"              // L3
-              + "\n"+""
-              + "\n"+    "    void testMethod() {"
-              + "\n"+    "      int startingA = n';" // ERROR
-              + "\n"+    "    }"
-              + "\n"+""
-              + "\n"+    "  } // end class"
-              ;
-  String[] args = {unitMarker, compileUnit};
-  TCompiler truejCompiler = compileFresh(args);
-  msgMustContain(truejCompiler, "A different final decoration, n, was defined at line 3");
+public void decorationDisagreesWithDecoratedInitialized_errorTest() throws Exception {
+  compileForError(
+      """
+      class TestClass {
+        int n' = 5;
+        int a, b = 3;           // ERROR LINE
+        int m' = 2;
+        int c';
+      } // end class
+      """ ,
+
+      """
+      Valuename a must be decorated to agree with the previous use of decorated final values
+      Valuename b must be decorated to agree with the previous use of decorated final values
+      """ );
+
 }
 
 @Test
-public void DecoratedFinalVariableUsedWithoutDecoration_errorTest() throws Exception {
-  String methodBody = "      int startingA' = 'a;" // L5
-                  + "\n      b' = startingA';"     // L6 OK
-                  + "\n      a  = startingA;"      // L7 ERROR
-                  ;
-  var truejCompiler = compileBody(methodBody);
-  msgMustContain(truejCompiler, "A different final decoration, startingA', was defined at line 5");
+public void UseDisagreesWithUndecoratedDeclaration_errorTest() throws Exception {
+  compileForError(
+    """
+    class TestClass {
+      int n = 5;                // L2
+
+      void testMethod() {
+        int startingA = n';   // ERROR n'
+        int x' = n;           // ERROR x'
+        int a  = n;
+        int b  = startingA;
+      }
+
+    } // end class
+    """ ,
+
+    """
+    A different final decoration, n, was defined at line 2
+    Valuename x' must be undecorated to agree with the previous use of undecorated final values
+    """);
 }
 
 @Test
-public void UndecoratedFinalVariableUsedWithDecoration_errorTest() throws Exception {
-  String methodBody = "      int startingA = 'a;" // L5
-              + "\n      b' = startingA;"         // L6
-              + "\n      a  = startingA';"        // L7
-              ;
+public void useDisagreesWithDecoratedLocalDeclaration_errorTest() throws Exception {
+  compileForError(
+    """
+    class TestClass {         // L1
+      int 'a, 'b;             // L2
+                              // L3
+      void testMethod() {     // L4
+        int startingA' = 'a;  // L5
+        b' = startingA';      // L6 OK
+        a  = startingA';      // L7 ERROR
+        int c' = startingA;   // L8 ERROR
+      }
+
+    } // end class
+    """ ,
+
+    """
+    Valuename a must be decorated to agree with the previous use of decorated final values
+    A different final decoration, startingA', was defined at line 5
+    """);
+}
+
+@Test
+public void decoratedFinalLocalVariableWhenDecorationIsForbidden_errorTest() throws Exception {
+  compileForError(
+    """
+    class TestClass {         // L1
+      int 'a, 'b;             // L2
+                              // L3
+      void testMethod() {     // L4
+        int startingA = 'a;   // L5
+        b  = startingA;       // L6 OK
+        a' = startingA;       // L7 ERROR
+        int c = startingA';   // L8 ERROR
+      }
+
+    } // end class
+    """ ,
+
+    """
+    Valuename a' must be undecorated to agree with the previous use of undecorated final values
+    A different final decoration, startingA, was defined at line 5
+    """);
+}
+
+@Test
+public void undecoratedFinalVariableUsedWithDecoration_errorTest() throws Exception {
+  String methodBody = "      int startingA = 'a;"       // L5
+                    + "\n      b' = startingA;"         // L6
+                    + "\n      a  = startingA';"        // L7
+                    ;
   var truejCompiler = compileBody(methodBody);
   msgMustContain(truejCompiler,
       "A different final decoration, startingA, was defined at line 5");
-}
-
-@Test
-public void UseFinalFieldWithoutDecoration_test() throws Exception {
-  String methodBody = "      int startingA = 'a;"
-              + "\n      b' = startingA;"
-              + "\n      a  = 'b;"
-              ;
-  var truejCompiler = compileBody(methodBody);
-  compileMustSucceed(truejCompiler);
-}
-
-@Test
-public void UseFinalVariableWithoutDecoration_test() throws Exception {
-  String methodBody = "      int startingA = 'a;"
-                  + "\n      b' = startingA;";
-  var truejCompiler = compileBody(methodBody);
-  compileMustSucceed(truejCompiler);
 }
 
 @Test
@@ -96,15 +145,14 @@ public void InitializeFinalVariableWithoutDecoration_test() throws Exception {
 
 @Test
 public void InitializeFinalFieldWithoutDecoration_test() throws Exception {
-  String methodBody = "      int startingA = 'a;";
   String unitMarker = "-unit";
   String compileUnit =   "  class TestClass {"
-              + "\n"+    "  int a, b;"
-              + "\n"+    "  int n = a + b;"
+              + "\n"+    "  int 'a = 1, 'b = 2;"
+              + "\n"+    "  int n = 'a + 'b;"
               + "\n"+""
               + "\n"+""
               + "\n"+    "    void testMethod() {"
-              + "\n"+    methodBody
+              + "\n"+    "      int testN = n+1;"
               + "\n"+    "    }"
               + "\n"+""
               + "\n"+    "  } // end class"
@@ -117,23 +165,63 @@ public void InitializeFinalFieldWithoutDecoration_test() throws Exception {
 private TCompiler compileBody(String methodBody) throws IOException, InterruptedException {
   String unitMarker = "-unit";
   String compileUnit =
-           "  class TestClass {"         // L1
-    + "\n"+"  int a, b;"                 // L2
-    + "\n"+ ""                           // L3
-    + "\n"+"    void testMethod() {"     // L4
-    + "\n"+ methodBody                   // L5
-    + "\n"+"    }"
-    + "\n"+""
-    + "\n"+"  } // end class"
-    ;
+    """
+    class TestClass {             // L1
+    int 'a, 'b;                     // L2
+                                  // L3
+      void testMethod() {         // L4
+    """
+    + methodBody +                // L5
+    """
+      }
+
+    } // end class
+    """ ;
   String[] args = {unitMarker, compileUnit};
   TCompiler truejCompiler = compileFresh(args);
   return truejCompiler;
 }
 
+private void compileForError(String compileUnit, String msg) throws Exception {
+  String unitArg = "-unit";
+  String[] args = {unitArg, compileUnit};
+  TCompiler truejCompiler = compileFresh(args);
+  theErrorMsgsContain(truejCompiler, msg);
+}
+
+private void compileForSuccess(String compileUnit) throws Exception {
+  String unitMarker = "-unit";
+  String[] args = {unitMarker, compileUnit};
+  TCompiler truejCompiler = compileFresh(args);
+  compileMustSucceed(truejCompiler);
+}
+
 private TCompiler compileFresh(String[] args) throws IOException, InterruptedException {
   TCompiler truejCompiler = TCompiler.runTrueJCompiler(args);
   return truejCompiler;
+}
+
+public void theErrorMsgsContain(TCompiler truejCompiler, String uniquePartOfErrMsgs) {
+  var expectedList = uniquePartOfErrMsgs.split("\\n"); // split on line ending
+  var msgs = truejCompiler.msgCollector();
+  var errLines = msgs.errLines();
+  int numberExpected = expectedList.length;
+  int numberActual = errLines.size();
+  if (numberExpected != numberActual)
+    assertTrue("Different number of messages in expected (" + numberExpected +") "
+                + "and actual ("+ numberActual +")."
+                + "\nExpected \n"+ uniquePartOfErrMsgs
+                + "\nActual \n "+ msgs.toString()
+                , false);
+  for (int i = 0; i < numberExpected; i++) {
+    if ( ! errLines.get(i).contains(expectedList[i]) )
+      assertTrue("An expected message was not contained in the corresponding actual message."
+                   + "\nExpected: "+ expectedList[i]
+                   + "\nActual  : "+ errLines.get(i)
+                   + "\n\nThe " + numberExpected +" Expected messages \n"+ uniquePartOfErrMsgs
+                   + "\n\nThe " + numberActual +" actual messages from "+ msgs.toString()
+                  , false);
+  }
 }
 
 private void msgMustContain(TCompiler truejCompiler, String msgSegment) {
@@ -145,7 +233,7 @@ private void msgMustContain(TCompiler truejCompiler, String msgSegment) {
 }
 
 private void compileMustSucceed(TCompiler truejCompiler) {
-  System.out.println("From compileMustSucceed\n"+ errorMsgs(truejCompiler));
+//  System.out.println("From compileMustSucceed\n"+ errorMsgs(truejCompiler));
   assertTrue(errorMsgs(truejCompiler)
               , truejCompiler.counts().noErrors());
 }
